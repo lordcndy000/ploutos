d90be1d5361986abe1660c7d599fe15da152967e
Lordwill Mabalot <lordwill.mabalot@august99.com>
2018-11-12 15:38:47 +0800

    Initial commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dashboard">
+    dashboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack')
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        data: `@import "@/scss/_variables.scss";`
+      }
+    }
+  },
+  configureWebpack: {
+    plugins: [
+
+      // ...
+
+      // Jquery loader plugin.
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+      })
+    ]
+  }
+}
